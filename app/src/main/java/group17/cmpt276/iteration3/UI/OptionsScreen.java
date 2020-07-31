1b84b16ea7c7a8efae7e46a4790145fb1a572b62
sahil <smodak@sfu.ca>
2020-07-30 18:01:02 -0700

    save and cancel buttons created

--- app/src/main/java/group17/cmpt276/iteration3/UI/OptionsScreen.java
+++ app/src/main/java/group17/cmpt276/iteration3/UI/OptionsScreen.java
@@ -15,6 +15,8 @@ public class OptionsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_screen);
+
+
     }
 
 }
