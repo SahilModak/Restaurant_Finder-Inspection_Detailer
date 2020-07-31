4bfb56e1af2975f0b1dd1cdff1cdd8e299c7fdec
sahil <smodak@sfu.ca>
2020-07-30 18:14:23 -0700

    singleton for search criteria added

---
+++ app/src/main/java/group17/cmpt276/iteration3/Model/SearchCriteria.java
@@ -0,0 +1,48 @@
+package group17.cmpt276.iteration3.Model;
+
+public class SearchCriteria {
+    private SearchCriteria instance;
+    private boolean searchFavourites = false;
+    private String searchHazardLevel = "";
+    private int maxViolations;
+    private int minViolations = 0;
+
+    private SearchCriteria getInstance(){
+        if(this.instance == null){
+            instance = new SearchCriteria();
+        }
+        return this.instance;
+    }
+
+    public boolean isSearchFavourites() {
+        return searchFavourites;
+    }
+
+    public String getSearchHazardLevel() {
+        return searchHazardLevel;
+    }
+
+    public int getMaxViolations() {
+        return maxViolations;
+    }
+
+    public int getMinViolations() {
+        return minViolations;
+    }
+
+    public void setSearchFavourites(boolean searchFavourites) {
+        this.searchFavourites = searchFavourites;
+    }
+
+    public void setSearchHazardLevel(String searchHazardLevel) {
+        this.searchHazardLevel = searchHazardLevel;
+    }
+
+    public void setMaxViolations(int maxViolations) {
+        this.maxViolations = maxViolations;
+    }
+
+    public void setMinViolations(int minViolations) {
+        this.minViolations = minViolations;
+    }
+}
