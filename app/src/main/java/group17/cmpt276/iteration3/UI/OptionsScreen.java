72e6470ebc8b4ece78fcb640b126d41d2a6ecb73
sahil <smodak@sfu.ca>
2020-07-30 18:05:46 -0700

    onclicklistenersadded

--- app/src/main/java/group17/cmpt276/iteration3/UI/OptionsScreen.java
+++ app/src/main/java/group17/cmpt276/iteration3/UI/OptionsScreen.java
@@ -3,7 +3,10 @@ package group17.cmpt276.iteration3.UI;
 import androidx.appcompat.app.AppCompatActivity;
 import group17.cmpt276.iteration3.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -15,8 +18,21 @@ public class OptionsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_screen);
+        Button btn1 = findViewById(R.id.saveBtn);
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(OptionsScreen.this, MainActivity.class));
+            }
+        });
 
-
+        Button btn2 = findViewById(R.id.saveBtn);
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(OptionsScreen.this, MainActivity.class));
+            }
+        });
     }
 
 }
