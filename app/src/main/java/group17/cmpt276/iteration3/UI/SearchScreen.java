4ce8e7c0ddd9d4cfc1e6bed0c42c1db911beaedf
= <=>
2020-08-05 18:30:41 -0700

    [jmt24] remove unnecessary class

--- app/src/main/java/group17/cmpt276/iteration3/UI/SearchScreen.java
+++ app/src/main/java/group17/cmpt276/iteration3/UI/SearchScreen.java
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import group17.cmpt276.iteration3.Model.NewDataNotify;
 import group17.cmpt276.iteration3.Model.RestaurantManager;
-import group17.cmpt276.iteration3.Model.SearchCriteria;
 import group17.cmpt276.iteration3.R;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
